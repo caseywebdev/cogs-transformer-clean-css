600944f30e9b2c809a55e8bc26974e95f2645bd1
Casey Foster <c@sey.me>
2016-10-18 21:28:27 -0500

    cogs@3, 3.0.0

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var CleanCSS = require('clean-css');
+const CleanCSS = require('clean-css');
 
-module.exports = function (file, options, cb) {
-  try {
-    var res = (new CleanCSS(options)).minify(file.buffer.toString());
-    var errorMessage = res.errors.concat(res.warnings).join('\n');
-    if (errorMessage) throw new Error(errorMessage);
-    cb(null, {buffer: new Buffer(res.styles + '\n')});
-  } catch (er) { return cb(er); }
+module.exports = ({file: {buffer}, options}) => {
+  const res = (new CleanCSS(options)).minify(buffer.toString());
+  const errorMessage = res.errors.concat(res.warnings).join('\n');
+  if (errorMessage) throw new Error(errorMessage);
+
+  return {buffer: new Buffer(`${res.styles}\n`)};
 };
--- test/test.js
+++ test/test.js
@@ -2,17 +2,7 @@ var helper = require('cogs-test-helper');
 
 helper.run({
   'test/config.json': {
-    'test/input.css': {
-      path: 'test/input.css',
-      buffer: helper.getFileBuffer('test/output.css'),
-      hash: helper.getFileHash('test/output.css'),
-      requires: [{
-        path: 'test/input.css',
-        hash: helper.getFileHash('test/input.css')
-      }],
-      links: [],
-      globs: []
-    },
+    'test/input.css': helper.getFileBuffer('test/output.css'),
     'test/error.css': Error
   }
 });
